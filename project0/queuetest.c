83ef16208225a6acee35a81759a13e65e1407e13
Conor Reisman <conorreisman@gmail.com>
2013-04-02 02:01:11 -0700

    Expanded test case to handle queue_is_empty and queue_remove.

--- project0/queuetest.c
+++ project0/queuetest.c
@@ -1,5 +1,8 @@
+#include <assert.h>
 #include <stdio.h>
 #include "queue.h"
+
+#define LENGTH(x) (sizeof(x)/sizeof(*(x)))
 
 // Print out the index and the value of each element.
 bool show_one(queue_element* elem, queue_function_args* args) {
@@ -8,20 +11,47 @@ bool show_one(queue_element* elem, queue_function_args* args) {
   return true;
 }
 
+static void print_queue(queue* q) {
+  int index = 0;
+  queue_apply(q, show_one, &index);
+  printf("Queue size is %zu\n\n", queue_size(q));
+}
+
 int main(int argc, char* argv[]) {
   queue* q = queue_create();
+
+  assert(queue_is_empty(q));
 
   int x = 0;
   int y = 1;
   int z = 2;
-  queue_append(q, &x);
-  queue_append(q, &y);
-  queue_append(q, &z);
-  queue_append(q, &x);
-  printf("Queue size is %zu\n", queue_size(q));
 
-  int index = 0;
-  queue_apply(q, show_one, &index);
+  int* elems[] = {&x, &y, &z, &x};
+  int size = LENGTH(elems);
+
+  int i;
+  for (i = 0; i < size; i++) {
+    queue_append(q, elems[i]);
+    assert(!queue_is_empty(q));
+  }
+
+  print_queue(q);
+  assert(size == queue_size(q));
+
+  for (i = 0; i < size; i++) {
+    assert(!queue_is_empty(q));
+    
+    int* ret;
+    assert(queue_remove(q, (queue_element**) &ret));
+    assert(ret == elems[i]);
+    assert(size - 1 - i == queue_size(q));
+    
+    print_queue(q);
+  }
+
+  assert(queue_is_empty(q));
+
+  free(q);
 
   return 0;
-}
+} 
