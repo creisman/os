ff79781ae6637e9833f646c6c9352f592b8e6938
Conor Reisman <conorreisman@gmail.com>
2013-04-02 14:55:43 -0700

    Added reverse and sort tests.

--- project0/queuetest.c
+++ project0/queuetest.c
@@ -1,14 +1,31 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include "queue.h"
 
 #define LENGTH(x) (sizeof(x)/sizeof(*(x)))
+
+static void test_reverse_empty();
+static void test_reverse_single();
+static void test_reverse();
+
+static void test_sort_empty();
+static void test_sort_single();
+static void test_sort();
 
 // Print out the index and the value of each element.
 bool show_one(queue_element* elem, queue_function_args* args) {
   printf("Item %d == %d\n", *(int*) args, *(int*) elem);
   *(int*) args = *(int*) args + 1;
   return true;
+}
+
+int compare_ints(const void* a, const void* b) {
+  return *(int*) a - *(int*) b;
+}
+
+int compare_elements(queue_element* a, queue_element* b) {
+  return *(int*) a - *(int*) b;
 }
 
 static void print_queue(queue* q) {
@@ -51,7 +68,112 @@ int main(int argc, char* argv[]) {
 
   assert(queue_is_empty(q));
 
+  int* ret;
+  assert(!queue_remove(q, (queue_element**) &ret));
+
   free(q);
 
+  test_reverse_empty();
+  test_reverse_single();
+  test_reverse();
+
+  test_sort_empty();
+  test_sort_single();
+  test_sort();
+
   return 0;
-} 
+}
+
+static void test_reverse_empty() {
+  queue* q = queue_create();
+
+  queue_reverse(q);
+  assert(queue_is_empty(q));
+}
+
+static void test_reverse_single() {
+  queue* q = queue_create();
+
+  int x = 10;
+  queue_append(q, &x);
+  queue_reverse(q);
+
+  int *ret;
+  queue_remove(q, (queue_element **) &ret);
+
+  assert(x = *(int*) ret);
+
+  assert(queue_is_empty(q));
+}
+
+static void test_reverse() {
+  const int SIZE = 20;
+
+  queue* q = queue_create();
+
+  int* vals = malloc(sizeof(int) * SIZE);
+  for (int i = 0; i < SIZE; i++) {
+    vals[i] = i;
+  }
+
+  for (int i = 0; i < SIZE; i++) {
+    queue_append(q, (queue_element*) (vals + i));
+  }
+
+  queue_reverse(q);
+  print_queue(q);
+
+  for (int i = SIZE - 1; i >= 0; i--) {
+    int* ret;
+    assert(queue_remove(q, (queue_element**) &ret));
+    assert(vals[i] == *(int*) ret);
+  }
+
+  assert(queue_is_empty(q));
+
+  free(q);
+}
+
+static void test_sort_empty() {
+  queue* q = queue_create();
+
+  queue_sort(q, compare_elements);
+  assert(queue_is_empty(q));
+}
+
+static void test_sort_single() {
+  queue* q = queue_create();
+
+  int x = 10;
+  queue_append(q, &x);
+  queue_sort(q, compare_elements);
+
+  int *ret;
+  queue_remove(q, (queue_element **) &ret);
+
+  assert(x = *(int*) ret);
+
+  assert(queue_is_empty(q));
+}
+
+static void test_sort() {
+  int unsorted[] = { 2, 4, 3, 18, 6, 6, 5, 3 };
+  int size = LENGTH(unsorted);
+  int* sorted = malloc(size * sizeof(sorted[0]));
+  memcpy(sorted, unsorted, sizeof(unsorted[0]) * size);
+  qsort(sorted, size, sizeof(sorted[0]), compare_ints);
+
+  queue* q = queue_create();
+
+  for (int i = 0; i < size; i++) {
+    queue_append(q, (queue_element*) (unsorted + i));
+  }
+
+  queue_sort(q, compare_elements);
+
+  for (int i = 0; i < size; i++) {
+    int *ret;
+    queue_remove(q, (queue_element **) &ret);
+    assert(sorted[i] == *(int*) ret);
+  }
+}
